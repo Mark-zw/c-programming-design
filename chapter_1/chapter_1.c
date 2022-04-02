7d28c949098dfee71f7cdc025f15b6883f7177ae
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-02 22:23:00 +0800

    Update chapter_1.c

--- chapter_1/chapter_1.c
+++ chapter_1/chapter_1.c
@@ -109,3 +109,14 @@
 //	printf("max: %d\n", max);
 //	return 0;
 //}
+#include<stdio.h>
+int main()
+{
+	int i, mul = 1;
+	for (i = 1; i <= 5; ++i)
+	{
+		mul *= i;
+	}
+	printf("%d\n", mul);
+	return 0;
+}
