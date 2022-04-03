9c3448260979e84b7ccfa843f82fd8377409a645
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-03 23:33:16 +0800

    new

---
+++ chapter_3/chapter3.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//    printf("printf(\"Hello world!\\n\");\n");
+//    printf("cout << \"Hello world!\" << endl;\n");
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    float sick, death;
+//    float rate;
+//    scanf("%d%d", &sick, &death);
+//    rate = (death / sick) * 100;
+//    printf("%.3f%%", rate);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    printf("\"Genius is 1%% inspiration and 99%% perspiration.\"");
+//    return 0;
+//}
+#include<stdio.h>
+int main()
+{
+    printf("Welcome to ACM / ICPC!");
+    return 0;
+}
