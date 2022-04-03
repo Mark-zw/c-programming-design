bb4a9e34abf4213d08332d8c8d0c24d7a66ef670
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-04 00:09:36 +0800

    updated

--- chapter_3/chapter3.c
+++ chapter_3/chapter3.c
@@ -24,9 +24,50 @@
 //    printf("\"Genius is 1%% inspiration and 99%% perspiration.\"");
 //    return 0;
 //}
+//#include<stdio.h>
+//int main()
+//{
+//    printf("Welcome to ACM / ICPC!");
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	float f, c;
+//	scanf("%f", &f);
+//	c = (5.0 / 9.0) * (f - 32);
+//	printf("%.1f\n", c);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	float p0 = 1000, r1 = 0.0036, r2 = 0.0225, r3 = 0.0198, p1, p2, p3;
+//	p1 = p0 * (1 + r1);
+//	p2 = p0 * (1 + r2);
+//	p3 = p0 * (1 + r3 / 2) * (1 + r3 / 2);
+//	printf("p1 = %f\np2 = %f\np3 = %f\n", p1, p2, p3);
+//	return 0;
+//}
+
 #include<stdio.h>
 int main()
 {
-    printf("Welcome to ACM / ICPC!");
-    return 0;
+	//printf("'");
+	//printf("-----------------------------\n");
+	//printf("\'");
+	//printf("?\n");
+	//printf("-----------------------------\n");
+	//printf("\?\n");
+
+	printf("\a");
+	return 0;
 }
