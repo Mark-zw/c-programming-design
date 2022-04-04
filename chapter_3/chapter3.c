1ed64defb0ce0fa38da4cd95a32081a34b985489
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-04 15:10:27 +0800

    updated

--- chapter_3/chapter3.c
+++ chapter_3/chapter3.c
@@ -77,22 +77,121 @@
 //	return 0;
 //}
 
-#include<stdio.h>
-int main()
-{
-	int a = + 1, b = - 2, c = 3;
-	printf("(%d) + (%d) = %d\n", a, b, a + b);
-	printf("(%d) * (%d) = %d\n", a, b, a * b);
-	printf("(%d) / (%d) = %d\n", a, b, a / b);
-	printf("(%d) / (%d) = %d\n", b, a, b / a);
-	a = 3, b = 4;
-	printf("%d %% %d = %d\n", a, b, a % b);
-	printf(" ++a = %d\n", ++a);
-	printf(" ++b = %d\n", ++b);
-	printf(" a++ = %d\n", a++);
-	printf(" b++ = %d\n", b++);
-	printf(" a = %d\n", a);
-	printf(" b = %d\n", b);
+//#include<stdio.h>
+//int main()
+//{
+//	int a = + 1, b = - 2, c = 3;
+//	printf("(%d) + (%d) = %d\n", a, b, a + b);
+//	printf("(%d) * (%d) = %d\n", a, b, a * b);
+//	printf("(%d) / (%d) = %d\n", a, b, a / b);
+//	printf("(%d) / (%d) = %d\n", b, a, b / a);
+//	a = 3, b = 4;
+//	printf("%d %% %d = %d\n", a, b, a % b);
+//	printf(" ++a = %d\n", ++a);
+//	printf(" ++b = %d\n", ++b);
+//	printf(" a++ = %d\n", a++);
+//	printf(" b++ = %d\n", b++);
+//	printf(" a = %d\n", a);
+//	printf(" b = %d\n", b);
+//	return 0;
+//}
+//
+////给定大小字母，将其转换为小写字母输出
+//#include<stdio.h>
+//int main()
+//{
+//	char ch1, ch2;
+//	char blank;
+//	while (scanf("%c", &ch1) != EOF)
+//	{
+//		getchar();
+//		ch2 = ch1 + 32;
+//		//putchar(ch1);
+//		//putchar('-');
+//		//putchar('>');
+//		//putchar(ch2);
+//		//putchar('\n');
+//		printf("%c->%c\n", ch1, ch2);
+//	}
+//	return 0;
+//}
 
-	return 0;
-}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 1, b = 2, c;
+//	c = a + b;
+//	int d = 1 + c;
+//	printf("%d\n", d);
+//	return 0;
+//}
+
+////给出三角形的三边长，求三角形的面积
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	float a, b, c, p,area;
+//	scanf("%f%f%f", &a, &b, &c);
+//	if ((a + b) > c && (a + c) > b && (b + c) > a)
+//	{
+//		p = (a + b + c) / 2;
+//		area = sqrt(p * (p - a) * (p - b) * (p - c));
+//		printf("a = %f\tb = %f\tc = %f\n", a, b, c);
+//		printf("area is %f\n", area);
+//	}
+//	else
+//		printf("can not be a triangle!\n");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	printf("i = %d\n", i);
+//	printf("i = %5d\n", i);
+//	printf("%e\n", 123.456);
+//	int a = -1;
+//	printf("a = %d\na = %o\na = %x\n", a, a,a);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	putchar('B');
+//	putchar('O');
+//	putchar('Y');
+//	putchar('\n');
+//	int i = 1;
+//	while (i++ < 20)
+//		putchar('-');
+//	putchar('\n');
+//	int a = 66, b = 79, c = 89;
+//	putchar(a);
+//	putchar(b);
+//	putchar(c);
+//	putchar('\n');
+//	a = getchar();
+//	b = getchar();
+//	c = getchar();
+//	putchar(a);
+//	putchar(b);
+//	putchar(c);
+//	printf("%c\n", getchar());
+//	printf("%c\n", getchar());
+//	printf("%c\n", getchar());
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char ch1, ch2;
+//	ch1 = getchar();
+//	ch2 = ch1 + 32;
+//	putchar(ch2);
+//	return 0;
+//}
