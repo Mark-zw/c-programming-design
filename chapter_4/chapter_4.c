920c55959e603a68fd98a761be9faa132cc8c7b6
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-05 23:02:13 +0800

    updated

--- chapter_4/chapter_4.c
+++ chapter_4/chapter_4.c
@@ -4,13 +4,61 @@
 //{
 //	return 0;
 //}
+//#include<stdio.h>
+//int main()
+//{
+//    int year, month, day;
+//    scanf("%4d%2d%2d", &year, &month, &day);
+//    printf("year=%d\n", year);
+//    printf("month=%02d\n", month);
+//    printf("day=%02d\n", day);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    printf("%#o %#X", 1234,1234);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int a, b, c;
+//    scanf("%d%d%d", &a, &b, &c);
+//    printf("%d%8d%8d\n", a, b, c);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    float f;
+//    scanf("%f", &f);
+//    printf("%d", (int)f % 10);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    int X, N, d;
+//    scanf("%d%d", &X, &N);
+//    d = ((N % 7) + X) % 7;
+//    if (d == 0)
+//        d = 7;
+//    printf("%d", d);
+//}
+
 #include<stdio.h>
 int main()
 {
-    int year, month, day;
-    scanf("%4d%2d%2d", &year, &month, &day);
-    printf("year=%d\n", year);
-    printf("month=%02d\n", month);
-    printf("day=%02d\n", day);
-    return 0;
+	int x;
+	while (scanf("%d", &x) != EOF)
+	{
+		if (x >= 0)
+			printf("%d\n", x);
+		else
+			printf("%d\n", -x);
+	}
+	return 0;
 }
--- chapter_4/chapter_4.cpp
+++ chapter_4/chapter_4.cpp
@@ -5,3 +5,17 @@
 //{
 //	return 0;
 //}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//    //int n1 = 0xABCDEF;
+//    //int n2 = 0XABCDEF;
+//    //printf("%15d\n", n1);
+//    //printf("%15d\n", n2);
+//    printf("%15d\n", 0xABCDEF);
+//    printf("%15d\n", 0XABCDEF);
+//
+//    return 0;
+//}
