c96e529f436fa604c285523d3997977848621193
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-05 18:35:25 +0800

    updated

--- chapter_4/chapter_4.c
+++ chapter_4/chapter_4.c
@@ -1,6 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	return 0;
+//}
 #include<stdio.h>
 int main()
 {
-	return 0;
+    int year, month, day;
+    scanf("%4d%2d%2d", &year, &month, &day);
+    printf("year=%d\n", year);
+    printf("month=%02d\n", month);
+    printf("day=%02d\n", day);
+    return 0;
 }
