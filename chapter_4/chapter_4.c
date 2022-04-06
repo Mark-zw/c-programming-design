f3a4200ef7cb417e50b2bfce4dd3ed1f6e1db26b
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-06 09:12:48 +0800

    updated

--- chapter_4/chapter_4.c
+++ chapter_4/chapter_4.c
@@ -90,23 +90,153 @@
 //	return 0;
 //}
 
+//#include<stdio.h>
+//void Swap(double* x, double* y)
+//{
+//	double temp = *x;
+//	*x = *y;
+//	*y = temp;
+//}
+//int main()
+//{
+//	double a, b, c;
+//	scanf("%lf%lf%lf", &a, &b, &c);
+//	if (a > b)
+//		Swap(&a, &b);
+//	if (a > c)
+//		Swap(&a, &c);
+//	if (b > c)
+//		Swap(&b, &c);
+//	printf("%lf,%lf,%lf\n", a, b, c);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	//int max = (a > b) ? a : b;
+//	//int max;
+//	////max = (a > b) ? a : b;
+//	//(a > b) ? (max = a) : (max = b);
+//	//printf("%d\n", max);
+//	(a > b) ? printf("%d", a) : printf("%d", b);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char ch;
+//	scanf("%c", &ch);
+//	//if (ch >= 'A' && ch <= 'Z')
+//	//	ch += 32;
+//	//ch = (ch >= 'A' && ch <= 'Z') ? (ch + 32) : ch;
+//	(ch >= 'A' && ch <= 'Z') ? (ch += 32) : ch;
+//	printf("%c", ch);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char grade;
+//	scanf("%c", &grade);
+//	switch (grade)
+//	{
+//	case 'A':
+//		printf("85~100\n");
+//		break;
+//	case 'B':
+//		printf("70~84\n");
+//		break;
+//	case 'C':
+//		printf("60~69\n");
+//		break;
+//	case 'D':
+//		printf("< 60\n");
+//		break;
+//	default:
+//		printf("enter data error\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+////¼òÒ×¼ÆËãÆ÷
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	char ch = 0;
+//	printf("Please enter two integer you want to calculate:\n");
+//	scanf("%d%d", &a, &b);
+//	printf("Please choose the type of calculating from  + - * /:\n");
+//	getchar();
+//	scanf("%c", &ch);
+//	switch (ch)
+//	{
+//	case '+':
+//		printf("%d + %d = %d\n", a, b, a + b);
+//		break;
+//	case '-':
+//		printf("%d - %d = %d\n", a, b, a - b);
+//		break;
+//	case '*':
+//		printf("%d * %d = %d\n", a, b, a * b);
+//		break;
+//	case '/':
+//		printf("%d / %d = %d\n", a, b, a / b);
+//		break;
+//	default:
+//		printf("enter data error!\n");
+//		break;
+//	}
+//	return 0;
+//}
+
 #include<stdio.h>
-void Swap(double* x, double* y)
-{
-	double temp = *x;
-	*x = *y;
-	*y = temp;
-}
 int main()
 {
-	double a, b, c;
-	scanf("%lf%lf%lf", &a, &b, &c);
-	if (a > b)
-		Swap(&a, &b);
-	if (a > c)
-		Swap(&a, &c);
-	if (b > c)
-		Swap(&b, &c);
-	printf("%lf,%lf,%lf\n", a, b, c);
+	int c, s;
+	double p, w, d, f;
+	printf("please enter price and weight:\n");
+	scanf("%lf%lf", &p, &w);
+	printf("please enter distant:\n");
+	scanf("%d", &s);
+	if (s >= 3000)
+		c = 12;
+	else
+		c = s / 250;
+	switch (c)
+	{
+	case 0:
+		d = 0;
+		break;
+	case 1:
+		d = 2;
+		break;
+	case 2:
+	case 3:
+		d = 5;
+		break;
+	case 4:
+	case 5:
+	case 7:
+		d = 8;
+		break;
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+		d = 10;
+		break;
+	case 12:
+		d = 15;
+		break;
+	}
+	f = p * w * s * (1 - d / 100);
+	printf("freight is %lf\n", f);
 	return 0;
 }
