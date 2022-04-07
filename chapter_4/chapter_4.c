ef51b57fd00d0e433d0ad5a05dc91f990fdfff47
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-07 09:44:06 +0800

    updated

--- chapter_4/chapter_4.c
+++ chapter_4/chapter_4.c
@@ -382,3 +382,38 @@
 //	}
 //	return 0;
 //}
+
+////10.
+//#include<stdio.h>
+//int main()
+//{
+//	float bonus,profit;
+//	printf("Please enter you profit: ");
+//	scanf("%f", &profit);
+//	if (profit < 0)
+//		printf("error profit,try again,the number you input should larger than zero!\n");
+//	else if (profit <= 100000)
+//		bonus = profit * 0.1;
+//	else if (profit <= 200000)
+//		bonus = (profit - 100000) * 0.075 + 100000 * 0.1;
+//	else if (profit <= 400000)
+//		bonus = 100000 * 0.1 + 100000 * 0.075 + (profit - 200000) * 0.05;
+//	else if (profit <= 600000)
+//		bonus = 100000 * 0.1 + 100000 * 0.075 + 200000 * 0.05 + (profit - 400000) * 0.03;
+//	else if (profit <= 1000000)
+//		bonus = 100000 * 0.1 + 100000 * 0.075 + 200000 * 0.05 + 200000 * 0.03 + (profit - 600000) * 0.015;
+//	else
+//		bonus = 100000 * 0.1 + 100000 * 0.075 + 200000 * 0.05 + 200000 * 0.03 + 400000 * 0.015 + (profit - 1000000) * 0.01;
+//	printf("bonus is %.0f\n", bonus);
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+	float bonus, profit;
+	printf("Please input your profit(larger than zero): ");
+	scanf("%f", &profit);
+	int n = (int)profit / 100000;
+	return 0;
+}
