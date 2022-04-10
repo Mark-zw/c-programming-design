986dff7398983ee1af51b46bf7c1692e61278b9e
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-10 09:41:31 +0800

    updated

---
+++ chapter_7/chapter_7.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//void Print()
+//{
+//	printf("******************\n");
+//	printf("  How do you do!\n");
+//	printf("******************\n");
+//}
+//int main()
+//{
+//	Print();
+//	return 0;
+//}
+//
+//int Max(int x, int y)
+//{
+//	return x > y ? x : y;
+//}
+
+//#include<stdio.h>
+//int Max(int x, int y)
+//{
+//	return x > y ? x : y;
+//}
+//int main()
+//{
+//	int a, b;
+//	scanf("%d%d",&a,&b);
+//	printf("max is %d\n", Max(a, b));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	//int Sum(int, int);
+//	int Sum(int x, int y);
+//
+//	int a, b, sum;
+//	scanf("%d%d", &a, &b);
+//	sum = Sum(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//int Sum(int x, int y)
+//{
+//	return x + y;
+//}
+
