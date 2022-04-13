41e1e662c1d8f229f60752830a6e9887cd6f798c
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-13 09:14:01 +0800

    updated

--- chapter_7/chapter_7.c
+++ chapter_7/chapter_7.c
@@ -249,6 +249,8 @@
 //	return m;
 //}
 
+
+//1.求最大公约数和最小公倍数
 //#include<stdio.h>
 //int main()
 //{
@@ -262,33 +264,202 @@
 //	print_string(str);
 //	return 0;
 //}
-#include<stdio.h>
-int main()
-{
-	size_t GetGcd(int m, int n);
-	size_t GetLcm(int m, int n);
-	size_t a, b,gcd,lcm;//gcd greatest common divied 最大公约数 lcd lastest common multiple
-	printf("Please enter two positive integer:");
-	scanf("%d%d", &a, &b);
-	gcd = GetGcd(a, b);
-	lcm = GetLcm(a, b);
-	printf("%d 和 %d 的最大公约数：%d，最小公倍数是：%d\n",a,b, gcd, lcm);
-	return 0;
-}
-size_t GetGcd(size_t m, size_t n)
-{
-	//用辗转相除法求最大公约数
-	size_t r = m % n;
-	while (r)
-	{
-		m = n;
-		n = r;
-		r = m % n;
-	}
-	return n;
-}
-size_t GetLcm(int m, int n)
-{
-	size_t gcd = GetGcd(m, n);
-	return m * n / gcd;
-}
+//#include<stdio.h>
+//int main()
+//{
+//	size_t GetGcd(int m, int n);
+//	size_t GetLcm(int m, int n);
+//	size_t a, b,gcd,lcm;//gcd greatest common divied 最大公约数 lcd lastest common multiple
+//	printf("Please enter two positive integer:");
+//	scanf("%d%d", &a, &b);
+//	gcd = GetGcd(a, b);
+//	lcm = GetLcm(a, b);
+//	printf("%d 和 %d 的最大公约数：%d，最小公倍数是：%d\n",a,b, gcd, lcm);
+//	return 0;
+//}
+//size_t GetGcd(size_t m, size_t n)
+//{
+//	//用辗转相除法求最大公约数
+//	size_t r = m % n;
+//	while (r)
+//	{
+//		m = n;
+//		n = r;
+//		r = m % n;
+//	}
+//	return n;
+//}
+//size_t GetLcm(int m, int n)
+//{
+//	size_t gcd = GetGcd(m, n);
+//	return m * n / gcd;
+//}
+//
+////2.二次方程的解
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	void fun1(float x, float y, float z, float* p1, float* p2);
+//	void fun2(float x, float y, float z, float* p1, float* p2);
+//	void fun3();
+//	float a, b, c, delta, x1, x2;
+//	printf("Please enter three real number:");
+//	scanf("%f%f%f", &a, &b, &c);
+//	if (a == 0)
+//	{
+//		if (b != 0)
+//			printf("Have one solution: %f\n", c / b);
+//		else
+//			printf("No solution!\n");
+//	}
+//	else
+//	{
+//		delta = b * b - 4 * a * c;
+//		if (delta > 0)
+//		{
+//			fun1(a, b, c, &x1, &x2);
+//			printf("Have two solutions: roo1 = %f,root2 = %f\n", x1, x2);
+//		}
+//		else if (delta == 0)
+//		{
+//			fun2(a, b, c, &x1, &x2);
+//			printf("Have same solution: root1 = root2 = %f\n", x1);
+//		}
+//		else
+//			fun3();
+//	}
+//	return 0;
+//}
+//
+//void fun1(float x, float y, float z, float* p1, float* p2)
+//{
+//	float delta = y * y - 4 * x * z;
+//	*p1 = (-y + sqrt(delta)) / (2 * x);
+//	*p2 = (-y - sqrt(delta)) / (2 * x);
+//}
+//void fun2(float x, float y, float z, float* p1, float* p2)
+//{
+//	float delta = y * y - 4 * x * z;
+//	*p1 = *p2 = (-y) / (2 * x);
+//}
+//void fun3()
+//{
+//	printf("No solution!\n");
+//}
+
+//#include<stdio.h>
+//#include<math.h>
+//int IsPrime(int n)
+//{
+//	int i;
+//	for (i = 2; i <= sqrt(n); i++)
+//	{
+//		if (n % i == 0)
+//			break;
+//	}
+//	if (i <= sqrt(n))
+//		return 0;
+//	else
+//		return 1;
+//}
+//int main()
+//{
+//	int n,ret;
+//	scanf("%d", &n);
+//	ret = IsPrime(n);
+//	if (ret == 1)
+//		printf("%d is a prime\n", n);
+//	else
+//		printf("%d isn't a prime\n", n);
+//	return 0;
+//}
+
+////4.行列式转置
+//#include<stdio.h>
+//void Matrix_reverse(int a[3][3])
+//{
+//	int b[3][3];
+//	int i, j;
+//	//1.转置行列式，关键点 b[i][j] = a[j][i] 转置后，行变成列，列变成行
+//	for (i = 0; i < 3; i++)
+//		for (j = 0; j < 3; j++)
+//			b[i][j] = a[j][i];
+//	//2.拷贝回原数组
+//	for (i = 0; i < 3; i++)
+//		for (j = 0; j < 3; j++)
+//			a[i][j] = b[i][j];
+//}
+//void Matrix_print(int a[3][3])
+//{
+//	int i, j;
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 3; j++)
+//			printf("%-3d", a[i][j]);
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int a[3][3] = { 1,2,3,4,5,6,7,8,9 };
+//	printf("Before reverse\n");
+//	Matrix_print(a);
+//	Matrix_reverse(a);
+//	printf("----------------------------\n");
+//	printf("After reverse\n");
+//	Matrix_print(a);
+//	return 0;
+//}
+
+//5.逆置字符串
+//#include<stdio.h>
+//void String_reverse(char* str)
+//{
+//	int len = strlen(str);//求出字符串的有效字符长度
+//	int left = 0, right = len - 1;//left、right是左右下标
+//	while (left < right)
+//	{
+//		//将left、right元素互换
+//		char temp = str[left];
+//		str[left++] = str[right];
+//		str[right--] = temp;
+//	}
+//}
+//int main()
+//{
+//	char str[100];
+//	gets(str);
+//	printf("Before reverse: ");
+//	puts(str);
+//	String_reverse(str);
+//	printf(" After reverse: ");
+//	puts(str);
+//	return 0;
+//}
+
+////6.字符串连接
+//#include<stdio.h>
+//#include<string.h>
+//void String_connect(char* str1, char* str2)
+//{
+//	//计算出两个字符串的有效长度
+//	int len1 = strlen(str1), len2 = strlen(str2);
+//	int begin1 = len1, begin2 = 0, end2 = len2;//begin1表示str1的开始，begin2、end2表示str2的开始和结束
+//	while (begin2 <= end2)
+//	{
+//		str1[begin1++] = str2[begin2++];
+//	}
+//}
+//int main()
+//{
+//	char str1[100], str2[30];
+//	gets(str1);
+//	gets(str2);
+//	printf("Before connect,string is %s\n",str1);
+//	String_connect(str1, str2);
+//	printf(" After connect,string is %s\n", str1);
+//	return 0;
+//}
+
+//7.复制元音字母
