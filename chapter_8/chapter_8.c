95955d91e7f890de0279b62c14a8786e56a1e857
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-14 18:00:45 +0800

    updated

--- chapter_8/chapter_8.c
+++ chapter_8/chapter_8.c
@@ -1,1 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	float b = 0;
+	//int* pa;
+	//float* pb;
+	//pa = &a;
+	//pb = &b;
+	int* pa = &a;
+	float* pb = &b;
+	return 0;
+}
