0586650ea8a7ac729d987dd95745c24d6efa0d85
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-15 08:13:11 +0800

    updated

--- chapter_8/chapter_8.c
+++ chapter_8/chapter_8.c
@@ -75,6 +75,21 @@
 #include<stdio.h>
 int main()
 {
-
+	int arr[3][4] = { 1,3,5,7,9,11,13,15,17,19,21,23 };
+	int* p;
+	for (p = arr[0]; p < arr[0] + 12; p++)
+	{
+		if ((p - arr[0]) % 4 == 0)
+			printf("\n");
+		printf("%4d", *p);
+	}
+	printf("\n");
+	for (p = arr[0]; p < arr[0] + 12; p++)
+	{
+		if ((p - arr[0]) % 4 == 0)
+			printf("\n");
+		printf("%p", p);
+	}
+	printf("\n");
 	return 0;
 }
