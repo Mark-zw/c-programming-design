97086580862ea6df9b6c07bd50e1aaedda979449
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2022-04-15 14:04:47 +0800

    updated

--- chapter_8/chapter_8.c
+++ chapter_8/chapter_8.c
@@ -164,14 +164,14 @@
 //	return 0;
 //}
 
-#include<stdio.h>
-int main()
-{
-	char a[] = "I am a boy.", b[20];
-	char* p1 = a, * p2 = b;
-	for (int i = 0; *(p1) != '\0'; p1++, p2++)
-		*p2 = *p1;
-	*p2 = '\0';
-	printf("%s", b);
-	return 0;
-}
+//#include<stdio.h>
+//int main()
+//{
+//	char a[] = "I am a boy.", b[20];
+//	char* p1 = a, * p2 = b;
+//	for (int i = 0; *(p1) != '\0'; p1++, p2++)
+//		*p2 = *p1;
+//	*p2 = '\0';
+//	printf("%s", b);
+//	return 0;
+//}
