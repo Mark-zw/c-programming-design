2bb1152e4beb8479f4426db49e17aafc775d5b2e
Student Zhang <79700308+Mark-zw@users.noreply.github.com>
2022-04-20 12:15:11 +0800

    updated

--- chapter_8/test_8_2.c
+++ chapter_8/test_8_2.c
@@ -1,6 +1,34 @@
-#include<stdio.h>
+//#include<stdio.h>
+//int main()
+//{
+//
+//	return 0;
+//}
+#include<iostream>
+using namespace std;
 int main()
 {
-
-	return 0;
+    char ch;
+    while ((scanf("%c", &ch)) != EOF)
+    {
+        getchar();//½«\n¶Á×ß
+        switch (ch)
+        {
+        case 'A':
+        case 'a':
+        case 'E':
+        case 'e':
+        case 'I':
+        case 'i':
+        case 'O':
+        case 'o':
+        case 'U':
+        case 'u':
+            cout << "Vowel" << endl;
+            break;
+        default:
+            cout << "Consonant" << endl;
+        }
+    }
+    return 0;
 }
