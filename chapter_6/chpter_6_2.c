6cf7c9303617f1bf0a67b301d4ed284ac624e36e
Student Zhang <79700308+Mark-zw@users.noreply.github.com>
2022-04-22 21:35:44 +0800

    updated

--- chapter_6/chpter_6_2.c
+++ chapter_6/chpter_6_2.c
@@ -35,6 +35,21 @@
 //	return 0;
 //}
 
+//#include<stdio.h>
+//int main()
+//{
+//    char ch;
+//    while ((scanf("%c", &ch)) != EOF)
+//    {
+//        getchar();
+//        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+//            printf("%c is an alphabet.\n", ch);
+//        else
+//            printf("%c is not an alphabet.\n", ch);
+//    }
+//    return 0;
+//}
+
 //9.ÕÛ°ë²éÕÒ
 #include<stdio.h>
 int main()
