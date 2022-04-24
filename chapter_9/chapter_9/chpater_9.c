c5d9bdbbfafdfa5352b6df60a53fb1a8d5374d1d
Student Zhang <79700308+Mark-zw@users.noreply.github.com>
2022-04-25 00:26:15 +0800

    updated

--- chapter_9/chapter_9/chpater_9.c
+++ chapter_9/chapter_9/chpater_9.c
@@ -1,1 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+////test.c文件，用于测试
+//#include<stdio.h>
+//#include<sys/types.h>
+//#include<unistd.h>
+//int main()
+//{
+//    int i;
+//    while (1)
+//    {
+//        pintf("PID: %d  ", geipid());
+//        printf("PPID: %d\n", getppid());
+//        sleep(1);
+//    }
+//    return 0;
+//}
