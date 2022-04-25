19b4a4901234eb109904fc7dd0b3c21d09515305
Student Zhang <79700308+Mark-zw@users.noreply.github.com>
2022-04-25 09:55:15 +0800

    updated

--- chapter_9/chapter_9/chpater_9.c
+++ chapter_9/chapter_9/chpater_9.c
@@ -14,3 +14,52 @@
 //    }
 //    return 0;
 //}
+//#include<stdio.h>
+//union f
+//{
+//	int i;
+//	char ch;
+//};
+//typedef struct Student
+//{
+//	char name[20];
+//	char sex[10];
+//	char telephone[12];
+//	char address[20];
+//	int num;
+//	int age;
+//}Student;
+//int main()
+//{
+//	//union f f1;
+//	//f1.i = 1;
+//	//int ret = f1.ch;
+//	//if (ret == 1)
+//	//	printf("小端字节序\n");
+//	//else
+//	//	printf("大端字节序\n");
+//	printf("%d\n", sizeof(Student));
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//struct Date
+//{
+//	int month;
+//	int day;
+//	int year;
+//};
+//struct Student
+//{
+//	int num;
+//	char name[20];
+//	char sex;
+//	int age;
+//	struct Date birthday;
+//	char addr[30];
+//};
+//int main()
+//{
+//	printf("%d\n", sizeof(struct Student));
+//	return 0;
+//}
