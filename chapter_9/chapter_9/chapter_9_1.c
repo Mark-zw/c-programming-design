6e59c27887c2d2bfc97b0269612c6724cd69895d
Student Zhang <79700308+Mark-zw@users.noreply.github.com>
2022-04-28 10:39:15 +0800

    updated

---
+++ chapter_9/chapter_9/chapter_9_1.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	printf("log2(n) = %lf\n", log2(n));
+//	printf("sqrt(n) = %lf\n", sqrt(n));
+//	printf("n = %d\n", n);
+//	printf("n*log2(n) = %lf\n", n*log2(n));
+//	printf("n^2 = %d\n", n * n);
+//	printf("n^3 = %d\n", n * n * n);
+//	printf("2^n = %d\n", (int)pow(2, n));
+//	int i = 1, x = 1;
+//	while (i <= n)
+//	{
+//		x *= i;
+//		i++;
+//	}
+//	printf("n! = %d\n", x);
+//	return 0;
+//}
