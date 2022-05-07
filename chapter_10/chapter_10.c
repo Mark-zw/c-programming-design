45e39935b98b004ea204e57768cccec6fcc9d5ad
Student Zhang <79700308+Mark-zw@users.noreply.github.com>
2022-05-07 12:39:05 +0800

    updated

--- chapter_10/chapter_10.c
+++ chapter_10/chapter_10.c
@@ -1,7 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-	FILE* fp = 
+	//1.open file
+	FILE* fp = fopen("test1.txt", "r");
+	if (fp == NULL)
+		printf("Open file failed\n");
+	else
+		printf("Open file success\n");
+	//char ch = getchar();
+	////2.read/write file
+	//while (ch != '#')
+	//{
+	//	fputc(ch, fp);
+	//	putchar(ch);
+	//	ch = getchar();
+	//}
+	FILE* fp1 = fopen("test2.txt", "w");
+	if (fp == NULL)
+		printf("Open file failed\n");
+	else
+		printf("Open file success\n");
+	char ch1 = fgetc(fp);
+	//2.read/write file
+	while (ch1 != -1)
+	{
+		fputc(ch1, fp1);
+		putchar(ch1);
+		ch1 = fgetc(fp);
+	}
+	//3.close file
+	if (fp)
+	{
+		fclose(fp);
+		if (fp == EOF)
+			printf("Close file failed\n");
+		else
+			printf("Close file success\n");
+	}
 	return 0;
 }
