1f5d69496bc2c67b17ac496955ad5e516838ecaa
Student Zhang <79700308+Mark-zw@users.noreply.github.com>
2022-05-23 10:38:43 +0800

    updated

--- chapter11/chapter11.c
+++ chapter11/chapter11.c
@@ -1,2 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#define PI 3.1415
+//#include<stdio.h>
+//#define PI 3.1415
+//int  main()
+//{
+//	double r, l, s, v;
+//	scanf("%lf", &r);
+//	l = 2 * PI * r;
+//	s = PI * r * r;
+//	v = (4.0 / 3) * PI * r * r * r;
+//	printf("l = %lf\ns = %lf\nv = %lf\n", l, s, v);
+//	return 0;
+//}
+
+#define S(a,b) a*b
